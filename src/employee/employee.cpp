b3db19f330a655837dbf1cf50f53058bc1ca5b73
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-05-17 00:11:00 -0400

    adds initial source files
    NOTES:
    adds the minimum code for building the project

---
+++ src/employee/employee.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+int main ()
+{
+	return 0;
+}
+
+/*
+
+PurchaseOrder					May 16, 2024
+
+source: src/employee/employee.cpp
+author: @misael-diaz
+
+Copyright (C) 2024 Misael Díaz-Maldonado
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+References:
+[0] https://en.cppreference.com/w/cpp
+
+*/
